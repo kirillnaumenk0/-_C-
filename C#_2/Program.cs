3a200dc0d7e8119e0d84fc01ab1200fde9a8f4cf
Kirill <kirillnaumenk0@mail.ru>
2022-08-05 18:02:08 +0600

    C#_ДЗ№2

---
+++ C#_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+int max = a;
+
+if(a > max ) max = a;
+if(b > max ) max = b;
+if(c > max ) max = c;
+
+Console.Write("max = " + max);
