3aaa9223d78e0f65a9a2069e9f3d8479f76ea813
Kirill <kirillnaumenk0@mail.ru>
2022-08-05 18:19:17 +0600

    C#_ДЗ_№3

---
+++ C#_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+      if(number % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
