f9ec2e244b6a9321c823095fe358900c4c29cd56
Kirill <kirillnaumenk0@mail.ru>
2022-08-05 18:49:56 +0600

    C#_ДЗ_№4

---
+++ C#_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while(i < N)
+{
+    if((N - i) % 2 == 0)
+    {
+        Console.WriteLine(N-i);
+    }
+    i += 1; 
+}
