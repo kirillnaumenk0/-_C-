9d13d62feae45c4d7c5ffba8d0bd88f8eede1ba3
Kirill <kirillnaumenk0@mail.ru>
2022-08-05 17:46:21 +0600

    С#_1Первое домашнее задание

---
+++ C#_1/Program.cs
@@ -0,0 +1,18 @@
+﻿int min = 0;
+int max = 0;
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    max += a;
+    min += b;
+}
+else
+{
+    max += b;
+    min += a;
+}
+Console.WriteLine("max = " + max);
+Console.WriteLine("min = " + min);
